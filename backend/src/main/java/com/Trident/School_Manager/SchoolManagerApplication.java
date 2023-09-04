17ad5cd1e37019b8986f53a327497e0bfac71bf3
RASH1k-RAJ <pommyraj@gmail.com>
2023-09-04 18:53:32 +1000

    Initializing Repository

---
+++ backend/src/main/java/com/Trident/School_Manager/SchoolManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Trident.School_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagerApplication.class, args);
+	}
+
+}
